f3e31cc03eae1a6b07b16adbf6786146e4218c68
mikasa <mikasa5012@gmail.com>
2020-06-21 11:03:44 +0800

    Add a test

--- babelTest/plugins/babelHexRestore.js
+++ babelTest/plugins/babelHexRestore.js
@@ -1,17 +1,24 @@
 function hexRestorte(babel) {
     const { types: t, template } = babel;
     const visitor = {
-        "StringLiteral"(path) {
-            let {value, extra, type} = path.node
-            // console.log(value);
-            extra.raw = '"' + value + '"';
-        },
-        "NumericLiteral"(path) {
-            let {value, extra, type} = path.node
-            // console.log(value);
-            extra.raw =  value ;
+        // "StringLiteral"(path) {
+        //     let {value, extra, type} = path.node;
+        //     console.log(extra);
+        //     if (typeof extra != 'undefined') {
+        //         extra.raw = '"' + value + '"';
+        //     }
+            
+        // },
+        // "NumericLiteral"(path) {
+        //     let {value, extra, type} = path.node
+        //     // console.log(value);
+        //     extra.raw =  value ;
+        // }
+        "StringLiteral|NumericLiteral"(path) {
+            delete path.node.extra
         }
     };
+        
     return {visitor};
   }
   
