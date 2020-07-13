d0cc9fcc99fa935d77818b28a769e781ecf42a11
mikasa <mikasa5012@gmail.com>
2020-07-13 12:29:26 +0800

    Create whileSwitch.js

---
+++ test/whileSwitch.js
@@ -0,0 +1,63 @@
+const parser = require('@babel/parser');
+const t = require('@babel/types');
+
+const generator = require('@babel/generator').default;
+const traverse = require('@babel/traverse').default;
+
+
+const jscode = `
+var t = 1;
+var s = 2;
+var arr = "3|0|1|2|4".split("|");
+var cnt = 0;
+while (true) {
+    switch (arr[cnt++]) {
+    case "0":
+        console.log("This is case-block 0")
+        continue;
+    case "1":
+        console.log("This is case-block 1");
+        continue;
+    case "2":
+        console.log("This is case-block 2");
+        continue;
+    case "3":
+        console.log("This is case-block 3");
+        continue;
+    case "4":
+        console.log("This is case-block 4");
+        continue;
+    }
+    break;
+}
+var tt = 6;`
+
+
+const visitor = {
+    WhileStatement: {
+        enter: [
+            whileSwitch,
+        ]
+    }
+};
+
+function whileSwitch(path) {
+    const {test,body} = path.node;
+    if (!t.isBooleanLiteral(test) || test.value !== true) return;
+    if (body.body.length === 0  || !t.isSwitchStatement(body.body[0])) return;
+    let switch_state = body.body[0];
+    let {discriminant,cases} = switch_state;
+    if (!t.isMemberExpression(discriminant) || !t.isUpdateExpression(discriminant.property)) return;
+    let arr_name = discriminant.object.name;
+    let arr = [];
+    let all_pre_siblings = path.getAllPrevSiblings();
+    let tt = path.get('test');
+    let aa = path.getCompletionRecords();
+    console.log(aa[0].toString());
+}
+
+var ast = parser.parse(jscode);
+traverse(ast, visitor);
+
+var finCode = generator(ast);
+// console.log(finCode.code);
